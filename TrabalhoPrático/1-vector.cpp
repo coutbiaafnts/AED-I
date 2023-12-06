18f2db9eaffbd3b5c945e328dde6a262c8889a45
coutbiaafnts <bia.fontes653@gmail.com>
2023-12-06 19:34:25 -0300

    ADD: function welcome to the vector code and menu

--- TrabalhoPrático/1-vector.cpp
+++ TrabalhoPrático/1-vector.cpp
@@ -1,21 +1,76 @@
 #include <iostream>
 #include <windows.h>
-#include <time.h> 
+#include <time.h>
 #include <chrono>
 #include <vector>
 
 using namespace std;
 using namespace chrono;
 
-int main() {
+int welcome();
+
+int main()
+{
     steady_clock::time_point startTime = steady_clock::now();
-    setlocale(LC_ALL, "pt_BR.UTF-8");
     srand(time(NULL));
 
+    UINT CPAGE_UTF8 = 65001;
+    SetConsoleOutputCP(CPAGE_UTF8);
 
+    HANDLE colors = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(colors, 14);
+
+    system("cls");
+
+    int optionMenu;
+
+    optionMenu = welcome();
+
+    switch (optionMenu)
+    {
+    case 1:
+        // Adicionar despesa
+        break;
+    case 2:
+        // Listar despesas
+        break;
+    case 3:
+        // Editar despesa
+        break;
+    case 4:
+        // Remover despesa
+        break;
+    case 5:
+        return 0;
+    default:
+        cout << endl
+             << "!ERROR: Opção inválida." << endl;
+        Sleep(1500);
+        main();
+    }
 
     steady_clock::time_point endTime = steady_clock::now();
     duration<double> time = duration_cast<duration<double>>(endTime - startTime);
-    cout << endl << "-----------------------------------------------------------" << endl << "Tempo executando: (" << time.count() << ") segundos" << endl;
+    cout << endl
+         << "-----------------------------------------------------------" << endl
+         << "Tempo executando: (" << time.count() << ") segundos" << endl;
     return 0;
+}
+
+int welcome()
+{
+    int option;
+
+    cout << ">>--------------------------------- REGISTRO DE DESPESAS ---------------------------------<<" << endl;
+
+    cout << "1. Adicionar despesa" << endl;
+    cout << "2. Listar despesas" << endl;
+    cout << "3. Editar despesa" << endl;
+    cout << "4. Remover despesa" << endl;
+    cout << "5. Sair" << endl
+         << endl;
+    cout << "Digite uma opção: ";
+    cin >> option;
+
+    return option;
 }
--- TrabalhoPrático/3-queue.cpp
+++ TrabalhoPrático/3-queue.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <windows.h>
-#include <time.h> 
-#include <chrono> 
+#include <time.h>
+#include <chrono>
 #include <queue>
 
 using namespace std;
 using namespace chrono;
 
-int main() {
-    steady_clock::time_point startTime = steady_clock::now();
-    setlocale(LC_ALL, "pt_BR.UTF-8");
-    srand(time(NULL));
+int main()
+{
+   steady_clock::time_point startTime = steady_clock::now();
+   srand(time(NULL));
 
+   UINT CPAGE_UTF8 = 65001;
+   SetConsoleOutputCP(CPAGE_UTF8);
 
+   HANDLE colors = GetStdHandle(STD_OUTPUT_HANDLE);
+   SetConsoleTextAttribute(colors, 14);
 
-    steady_clock::time_point endTime = steady_clock::now();
-    duration<double> time = duration_cast<duration<double>>(endTime - startTime);
-    cout << endl << "-----------------------------------------------------------" << endl << "Tempo executando: (" << time.count() << ") segundos" << endl;
-    return 0;
+   steady_clock::time_point endTime = steady_clock::now();
+   duration<double> time = duration_cast<duration<double>>(endTime - startTime);
+   cout << endl
+        << "-----------------------------------------------------------" << endl
+        << "Tempo executando: (" << time.count() << ") segundos" << endl;
+   return 0;
 }
