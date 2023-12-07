e9978552e155ffc0182d6e3e9cf32be09a6ea72f
BIANCA DE CASTRO AGUIAR FONTES <0077064@IFMG.EDU.BR>
2023-12-07 09:34:33 -0300

    ADD: vector

--- TrabalhoPrático/1-vector.cpp
+++ TrabalhoPrático/1-vector.cpp
@@ -8,17 +8,18 @@ using namespace std;
 using namespace chrono;
 
 int welcome();
+void addExpense(), listExpense(), editExpense(), removeExpense();
+
+vector<int> expenses;
+vector<int>::iterator numb;
 
 int main()
 {
-    steady_clock::time_point startTime = steady_clock::now();
-    srand(time(NULL));
-
     UINT CPAGE_UTF8 = 65001;
     SetConsoleOutputCP(CPAGE_UTF8);
 
     HANDLE colors = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(colors, 14);
+    SetConsoleTextAttribute(colors, 11);
 
     system("cls");
 
@@ -29,14 +30,14 @@ int main()
     switch (optionMenu)
     {
     case 1:
-        // Adicionar despesa
+        addExpense();
         break;
     case 2:
-        // Listar despesas
-        break;
+        listExpense();
+        main();
     case 3:
-        // Editar despesa
-        break;
+        editExpense();
+        main();
     case 4:
         // Remover despesa
         break;
@@ -49,11 +50,6 @@ int main()
         main();
     }
 
-    steady_clock::time_point endTime = steady_clock::now();
-    duration<double> time = duration_cast<duration<double>>(endTime - startTime);
-    cout << endl
-         << "-----------------------------------------------------------" << endl
-         << "Tempo executando: (" << time.count() << ") segundos" << endl;
     return 0;
 }
 
@@ -62,15 +58,75 @@ int welcome()
     int option;
 
     cout << ">>--------------------------------- REGISTRO DE DESPESAS ---------------------------------<<" << endl;
-
     cout << "1. Adicionar despesa" << endl;
     cout << "2. Listar despesas" << endl;
     cout << "3. Editar despesa" << endl;
     cout << "4. Remover despesa" << endl;
-    cout << "5. Sair" << endl
+    cout << "5. Sair" << endl;
+    cout << ">>----------------------------------------------------------------------------------------<<" << endl
          << endl;
     cout << "Digite uma opção: ";
     cin >> option;
 
     return option;
+}
+
+void addExpense()
+{
+    system("cls");
+    int opt, expense;
+
+    cout << ">>--------------------------------- ADICIONAR DESPESA ---------------------------------<<" << endl
+         << endl;
+    cout << "Informe o tipo da despesa (1. Ganho | 2. Gasto): ";
+    cin >> opt;
+
+    if (opt == 1)
+    {
+        cout << "Informe o valor: ";
+        cin >> expense;
+        expenses.push_back(expense);
+        main();
+    }
+    else if (opt == 2)
+    {
+        cout << "Informe o valor com um (-) antes do número: ";
+        cin >> expense;
+        expenses.push_back(expense);
+        main();
+    }
+    else if (opt != 1 && opt != 2)
+    {
+        system("cls");
+        cout << "!ERROR: Opção inválida." << endl;
+        Sleep(1500);
+        addExpense();
+    }
+}
+
+void listExpense()
+{
+    system("cls");
+    cout << ">>--------------------------------- LISTANDO DESPESAS ---------------------------------<<" << endl
+         << endl;
+    for (int i = 0; i < expenses.size(); i++)
+    {
+        cout << i + 1 << ". : R$" << expenses[i] << endl;
+    }
+    system("pause");
+}
+
+void editExpense()
+{
+    system("cls");
+
+    int number;
+
+    cout << ">>--------------------------------- EDITANDO DESPESAS ---------------------------------<<" << endl
+         << endl;
+    cout << "Informe a posição da despesa: ";
+    cin >> number;
+
+    numb = expenses.begin();
+    expenses.insert(numb, number);
 }
